3e556f18a3d34625bbb8d61c993213d876d29352
jeffreytakaki <jtakaki1@gmail.com>
2014-08-07 18:28:25 -0700

    working email

--- contact.php
+++ contact.php
@@ -1,5 +1,5 @@
 <?php
-$toEmail 		= "john@nemusbioscience.com"; //Replace it recipient email address
+$toEmail 		= "john@nemusbio.com"; //Replace it recipient email address.
 $subject 		= 'Web Inquiry'; //Subject line for emails
 
 //Let's clean harmful characters from raw POST data using PHP Sanitize filters.
